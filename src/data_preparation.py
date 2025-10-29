997b8741706471629fd87a3fe96102bff31714d6
Ruth Barbieri <rbarbierip@gmail.com>
2025-10-29 18:14:20 -0500

    Se agrega notebook de preparacion de datos

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -16,6 +16,6 @@ def prepare_data(df):
     X = df_dummies.drop(columns=["TARGET"])
     y = df_dummies["TARGET"]
     
-    # Separación de datos train y test
+    # Separación de datos train y test 80, 20
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
     return X_train, X_test, y_train, y_test
