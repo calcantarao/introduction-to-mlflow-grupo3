1ad55f856c864cf4fe93edb37bbb7f85dd010127
dante.toribio <danteca.dtc@gmail.com>
2025-10-29 15:13:34 -0500

    Add comments in model_registry

--- src/model_registry.py
+++ src/model_registry.py
@@ -1,11 +1,18 @@
 def register_model(model, model_name, n_estimators, accuracy):
     import mlflow
 
+    # Configuración de MLflow
     mlflow.set_tracking_uri("http://localhost:5000")
+
+    # Creación del experimento
     mlflow.set_experiment("Mi primer Modelo")
 
+    # Inicio de la sesión
     with mlflow.start_run():
+        # Log de parámetros
         mlflow.log_param("model_name", model_name)
         mlflow.log_param("n_estimators", n_estimators)
+        # Log de métricas
         mlflow.log_metric("accuracy", accuracy)
+        # Log del modelo
         mlflow.sklearn.log_model(model, "model")
