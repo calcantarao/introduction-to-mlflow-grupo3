0c30d4b3c71af26bea3349795df6cff93bb1e004
lyquispeh <lyquispeh@gmail.com>
2025-10-30 16:15:24 -0500

    Se agrega notebook de entrenamiento del modelo

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -1,7 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# Utiliza el modelo Random Forest para entrenamiento del modelo
+# Utiliza el algoritmo Random Forest en el entrenamiento del modelo. 
 def train_model(X_train, y_train, X_test, y_test):
     clf = RandomForestClassifier(n_estimators=50)
     clf.fit(X_train, y_train)
